8b964b247d6041bd9d9e0d65f7ca5bce059d45f3
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-02-25 17:32:11 -0500

    Finished tasks 1-3, creating a homepage and pizza form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import {Switch, Route, Link} from 'react-router-dom';
+import PizzaForm from './Components/PizzaForm';
+
 
 const App = () => {
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className ='App'>
+      <header>
+        <Link to = '/'>Home</Link>
+        <Link to = '/pizza' id ='order-pizza'>Order</Link>
+      </header>
+      <Switch>
+        <Route exact path = '/'>
+          <h1>Lambda Eats</h1>
+        </Route>
+        <Route path = '/pizza'>
+          <h1>Make Your Order</h1>
+          <PizzaForm/>
+        </Route>
+        <Route path = '/pizza/:slug'>
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/Components/PizzaForm.js
@@ -0,0 +1,24 @@
+import React, {useState,useEffect} from 'react';
+import {useParams} from 'react-router-dom'
+
+const PizzaForm = () =>{
+    
+    return(
+    <article>  
+    <form id = 'pizza-form'>
+        <label>
+            This is a form
+        </label>
+        <label>
+            
+        </label>
+        <label>
+            
+        </label>
+        <label>
+            
+        </label>
+    </form>
+    </article>)
+}
+export default PizzaForm;
