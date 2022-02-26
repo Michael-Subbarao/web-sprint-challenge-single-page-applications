7a71d7f47cea73a7c5702d7155fa357cab311947
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-02-25 19:17:30 -0500

    finished 8 of 9 tasks

--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -6,51 +6,92 @@ import axios from "axios";
 
 const PizzaForm = () =>{
     const { pizza } = useParams();
+    const [error,setError] = useState("name must be at least 2 characters"); 
     const [form,setForm] = useState({
         name: '',
-        accepted:false,
-        
+        ingredients: {
+            pepperoni: false,
+            olives:false,
+            ham:false,
+            pineapple:false
+        },
+        size:'',
+        otherInstructions: ''
     })
-    const validate = name=>{
-        name.length<2 ? "name must be at least 2 characters" : '';
+
+    const formChange = e =>{
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setForm({...form,[e.target.name]:value});
     }
+    const validateName = (name)=>name.length < 2 ? "name must be at least 2 characters" : ''
 
     const handelSubmit = (e)=>{
         form.preventDefault();
         axios.post('https://reqres.in/api/orders',e.target)
     }
     return(
-        <article>  
-          <form id = 'pizza-form'>
+        <article>
+            <div>
+            {error && <p>{error}</p>}
+            </div>  
+          <form onSubmit={(a)=>handelSubmit(a)} id='pizza-form'>
             <label>
                 Name:
                 <input type = "text" name = 'name' id ='name-input' value = {form.name}
+                 onChange={(e)=>{setForm({...form,name:e.target.value}); setError(validateName(e.target.value))}}
                 ></input>
                 <select id="size-dropdown">
                     <option value={'s'}>S</option>
                     <option value={'m'}>M</option>
                     <option value={'l'}>L</option>
-                    <option value={'XL'}>XL</option>
+                    <option value={'xl'}>XL</option>
                 </select>
                 <div id="ingredients">
-                    <input
+                    <label> Pepperoni
+                    <input 
                     type="checkbox"
                     name="pepperoni"
-                     />
+                    checked = {form.ingredients.pepperoni}
+                    onChange = {formChange}
+                    />
+                    </label>
+                    <label> Olives
                     <input
                     type="checkbox"
                     name="olives"
+                    checked = {form.ingredients.olives}
+                    onChange = {formChange}
                     />
+                    </label>
+                    <label> Ham
                     <input
                     type="checkbox"
                     name="ham"
+                    checked = {form.ingredients.ham}
+                    onChange = {formChange}
                     />
+                    </label>
+                    <label> Pineapple
                     <input
                     type="checkbox"
                     name="pineapple"
+                    checked = {form.ingredients.pineapple}
+                    onChange = {formChange}
                     />
-            </div>
+                    </label>
+                </div>
             </label>
+            <input
+            type="text"
+            id="special-text"
+            value={form.otherInstructions}
+            onChange={formChange}
+            />
+
+                <input
+                type="submit"
+                id="order-button"
+                />
            </form>
         </article>)
 }
